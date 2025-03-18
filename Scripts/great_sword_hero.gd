f04568246a592d1e0f19489bc348aa60d7c64e84
joeyjoe0030 <139891627+joeyjoe0030@users.noreply.github.com>
2025-03-17 22:48:40 -0400

    added movement to greatsword hero

---
+++ Scripts/great_sword_hero.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move left", "move right")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("Idle")
+
+	move_and_slide()
