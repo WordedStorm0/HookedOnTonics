d99ba02ab366e6d7bbe6f1a1486340847b7f23c5
WordedStorm0 <zerokaox@gmail.com>
2025-03-23 22:35:28 -0400

    Added player movement and started to make the greatsword script.

--- Scripts/great_sword_hero.gd
+++ Scripts/great_sword_hero.gd
@@ -7,20 +7,29 @@ const JUMP_VELOCITY = -200.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	# Get directional input for both X and Y axes
+	#Vector2 stores 2D data like the coordinates for position and direction of a
+	#2D object. Here we use it to store the player input for moving on the
+	#X axis (left and right) and the Y axis (up and down)
+	var input_vector = Vector2(
+		#move left, right, up, and down are WASD which we initialized in the Input map
+		Input.get_axis("move left", "move right"),
+		Input.get_axis("move up", "move down")
+	)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("move left", "move right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Normalize the input vector to ensure consistent speed in all directions
+	if input_vector.length() > 0:
+		#Normalized checks the vector length which keeps the speed consistent
+		#to left and right movement when you move diagonally 
+		input_vector = input_vector.normalized()
+		velocity = input_vector * SPEED
 		animated_sprite.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#move_toward sets the character's movement to 0 when there is no player
+		#input
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED * delta)
 		animated_sprite.play("Idle")
-
+	if Input.is_action_just_pressed("attack"):
+		$Sword.swing()
+	# Move and slide the character in 2D space
 	move_and_slide()
